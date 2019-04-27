40287c1aa021027d44fd4659876c2fd64f451692
Yuki Okuno <yuki02631@gmail.com>
2019-04-27 15:21:54 +0900

    finish first setting

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def setting
+  end
+
+  def email_update
+  end
+
+  def password_update
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:'static_pages#home'
+  controller :users do
+    resources :users do
+      member do
+        get :setting
+        patch :email_update, :password_update
+      end
+    end
+  end
+
 end
